68235ccbb58e46c1fc0c790cbbd368e967d62b91
daubilnik <121643997+danelanchik@users.noreply.github.com>
2024-01-14 15:26:47 +0500

    Create hackworld.c

---
+++ hackworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = dva + dva;
+    printf("%d", x);
+    vzlomat_pentagon;
+    hack!!!!;
+    trevoga - otkluchit;
+    ukrast dengi!!!!!;
+    return 0;
+}
